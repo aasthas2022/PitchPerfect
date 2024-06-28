480dfd7ed90a6609eeb272f4ea149bfdaebfc87d
AasthaPersonalGithub <aasthas2022@gmail.com>
2024-06-28 01:14:36 -0400

    add format support

--- src/components/CoverLetter.js
+++ src/components/CoverLetter.js
@@ -8,11 +8,29 @@ function CoverLetter({ content }) {
     alert('Cover letter copied to clipboard!');
   };
 
+  const formatContent = (text) => {
+    const boldPattern = /\*\*(.*?)\*\*/g;
+    const italicPattern = /\*(.*?)\*/g;
+    const underlinePattern = /__(.*?)__/g;
+    const bulletPattern = /(^|\n)\*\s(.*?)(?=\n|$)/g;
+
+    let formattedText = text;
+    formattedText = formattedText.replace(boldPattern, '<b>$1</b>');
+    formattedText = formattedText.replace(italicPattern, '<i>$1</i>');
+    formattedText = formattedText.replace(underlinePattern, '<u>$1</u>');
+
+    // Wrap bullet points in <ul> and <li> tags
+    formattedText = formattedText.replace(bulletPattern, '$1<li>$2</li>');
+    formattedText = formattedText.replace(/(<li>.*<\/li>)+/g, '<ul>$&</ul>');
+
+    return { __html: formattedText };
+  };
+
   return (
     <div className="cover-letter-column">
       <label>Generated Cover Letter:</label>
       <div className="textarea-container">
-        <textarea id="CoverLetter" readOnly value={content} />
+        <div id="CoverLetter" dangerouslySetInnerHTML={formatContent(content)} className="formatted-content" />
         <button onClick={copyToClipboard} className="copy-icon">
           <FaClipboard />
         </button>
