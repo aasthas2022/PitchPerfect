e2047a9c2a63bb5973a652ee29700c3eca24b0a2
AasthaPersonalGithub <aasthas2022@gmail.com>
2024-06-28 00:48:13 -0400

    clipboard and background color

--- src/components/CoverLetter.js
+++ src/components/CoverLetter.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import './CoverLetter.css';
+import { FaClipboard } from 'react-icons/fa';
 
 function CoverLetter({ content }) {
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(content);
+    alert('Cover letter copied to clipboard!');
+  };
+
   return (
     <div className="cover-letter-column">
       <label>Generated Cover Letter:</label>
-      <textarea id= "CoverLetter" readOnly value={content} />
+      <div className="textarea-container">
+        <textarea id="CoverLetter" readOnly value={content} />
+        <button onClick={copyToClipboard} className="copy-icon">
+          <FaClipboard />
+        </button>
+      </div>
     </div>
   );
 }
